f7c5741e227bb95e93f691889a4b431a23901b07
sandraNotTaken <155741111+sandraNotTaken@users.noreply.github.com>
2025-11-07 16:37:23 +0100

    Add files via upload

---
+++ Unit Converter/index.js
@@ -0,0 +1,20 @@
+let valueBox = document.getElementById("unit-value")
+let convLength = document.getElementById("conv-length")
+let convVolume = document.getElementById("conv-volume")
+let convMass = document.getElementById("conv-mass")
+let convertBtn = document.getElementById("convert-btn")
+
+let input = valueBox.value
+
+convertBtn.addEventListener("click", function() {
+        convLength.innerHTML = 
+        `${valueBox.value} meters = ${(valueBox.value * 3.281).toFixed(2)} feet | 
+        ${valueBox.value} feet = ${(valueBox.value * 0.305).toFixed(2)} meters`
+        
+        convVolume.innerHTML = 
+        `${valueBox.value} liters = ${(valueBox.value * 0.264).toFixed(2)} gallons | 
+        ${valueBox.value} gallons = ${(valueBox.value * 3.788).toFixed(2)} liters`
+        convMass.innerHTML = 
+        `${valueBox.value} kilos = ${(valueBox.value * 2.204).toFixed(2)} pounds | 
+        ${valueBox.value} pounds = ${(valueBox.value * 0.454).toFixed(2)} kilos`
+})
